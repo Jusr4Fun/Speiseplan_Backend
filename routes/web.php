4335d9dea6bc1bd76d29af0821843d7907ddf84f
DvorakDoCJD <ddvorak@fisi-hr.de>
2022-11-24 08:58:56 +0100

    removed old data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +16,3 @@ use App\Http\Controllers\EmailController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/send-email', [EmailController::class, 'index']);
